a09eb2e1ef915a3c82e653ec1e181e92aa233431
jackletter <1286317554@qq.com>
2017-02-17 20:59:18 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*********************************************
+ * 功能描述:自动定时服务管理端
+ * 创 建 人:胡庆杰
+ * 日    期:2016-6-8
+ * github:https://github.com/jackletter/CodeCreator
+ * 说明:提供可视化管理支持
+ * 
+ ********************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
